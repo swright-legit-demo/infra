9e413e265b741428c07d19526f7c4d68aa8db4a9
Shane Wright <shane.wright@gmail.com>
2026-02-13 17:44:05 -0500

    some important changes

--- main.tf
+++ main.tf
@@ -24,3 +24,19 @@ resource "aws_s3_bucket_public_access_block" "insecure" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.terraform_state.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.terraform_state.arn}/*"
+      },
+    ]
+  })
+}
