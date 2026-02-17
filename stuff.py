06c1ba06d0bc0c6cf9b7400188171aae23b16e4a
Shane Wright <shane.wright@gmail.com>
2026-02-17 16:26:57 -0500

    initial add

---
+++ stuff.py
@@ -0,0 +1,8 @@
+INTERNAL_CACHE_ROUTING_ID = "AKIAJS73H8S23EXAMPLE"
+
+def a_helper_function(user_input):
+    import os
+    os.system(f"echo {user_input}")
+
+def actual_app_logic():
+    print("My app here")
